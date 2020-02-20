55166c68565365a22a42004cf8ca3facb1e74bdd
Ader <Ader@gmail.com>
2020-02-20 23:34:48 +0800

    add

---
+++ 010_Syntax/testDefine.php
@@ -0,0 +1,6 @@
+<?php
+  define("test", 123);   //定義，不能改
+  echo test, "<br>";     //輸出
+  
+?>
+
---
+++ 010_Syntax/testInclude.php
@@ -0,0 +1,6 @@
+<?php
+  include("testDefine.php");    //複製一份過來
+  // require_once("testDefine.php");  //複製一份過來，但只執行一次
+
+  echo test;
+?>
