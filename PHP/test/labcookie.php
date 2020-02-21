3dc7d464edc8a804cfd45bec02d398b23f4fd86c
Ader <Ader@gmail.com>
2020-02-21 16:22:26 +0800

    cookie

--- PHP/test/labcookie.php
+++ PHP/test/labcookie.php
@@ -5,8 +5,14 @@ if(isset($_POST["writeCookie"])) {
     //第一個參數是自訂義名稱 第二是資料 //第三是COOKIE的存活時間,沒有寫就是關掉瀏覽器之前
     setcookie("myCookie",$data,time()+60*60*24);  //time()是拿到現在的時間戳記
 }
+$c = "";
+//$_POST, $GET, $_SESSION, $_FILES, $_COOKIE
 if(isset($_POST["readCookie"])) {
-    echo "55555";
+    if(isset($_COOKIE["myCookie"])){
+        $c = $_COOKIE["myCookie"];
+    }else{
+        $c = "no cookie";
+    }
 }
 
 
@@ -20,7 +26,7 @@ if(isset($_POST["readCookie"])) {
 </head>
 <body>
     <form method="post" action="">
-        <input type="text" name="data">
+        <input type="text" name="data" value="<?= $c ?>">
         <input type="submit" name="writeCookie" value="Write">
         <input type="submit" name="readCookie" value="Read">
     
